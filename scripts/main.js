1fb043c7a2eb5d53d5c38efec4635192bf2758cb
Ken Zhou <mailofrealcev@gmail.com>
2020-03-10 08:49:32 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
+/*
+	chaddd chat
+*/
 
+var canvas = document.querySelector('#game-canvas');
+var ctx = canvas.getContext('2d');
+
+var players = [];
+var speed = 1;
+var me = {x:0,y:0};
+players.push(me);
+
+var db = firebase.database();
+var playerRef = db.ref('players');
