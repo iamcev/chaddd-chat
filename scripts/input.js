b85350908439ffbf39aef4c2eda64b77b9e9bdc3
Ken Zhou <mailofrealcev@gmail.com>
2020-03-10 08:49:47 -0400

    Update input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,1 +1,12 @@
+var input = {
+	keys:{},
+	keydown: function (e) {
+		input.keys[e.key.toLowerCase()] = true;
+	},
+	keyup: function (e) {
+		input.keys[e.key.toLowerCase()] = false;
+	}
+};
 
+window.addEventListener('keydown', input.keydown);
+window.addEventListener('keyup', input.keyup);
